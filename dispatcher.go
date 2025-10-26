8d4b42aeddb13564dbb580a2fd216dc6c4a85178
Sriram-bb63 <invadersriram2002@gmail.com>
2025-10-26 23:36:41 +0530

    pass data by channel to goroutine

--- dispatcher.go
+++ dispatcher.go
@@ -1,51 +1,70 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
 	"regexp"
 )
 
-func dispatch(data map[string]string) {
+func dispatch() {
 	log.Println("Dispatcher called")
-	outputPayload := make(map[string]any)
+	for data := range dataChan {
+		outputPayload := make(map[string]any)
 
-	outputPayload["event"] = data["ev"]
-	outputPayload["event_type"] = data["et"]
-	outputPayload["app_id"] = data["id"]
-	outputPayload["user_id"] = data["uid"]
-	outputPayload["message_id"] = data["mid"]
-	outputPayload["page_title"] = data["t"]
-	outputPayload["page_url"] = data["p"]
-	outputPayload["browser_language"] = data["l"]
-	outputPayload["screen_size"] = data["sc"]
-	outputPayload["attributes"] = make(map[string]map[string]string)
-	outputPayload["traits"] = make(map[string]map[string]string)
-	attributesMap := outputPayload["attributes"].(map[string]map[string]string)
-	traitsMap := outputPayload["traits"].(map[string]map[string]string)
+		outputPayload["event"] = data["ev"]
+		outputPayload["event_type"] = data["et"]
+		outputPayload["app_id"] = data["id"]
+		outputPayload["user_id"] = data["uid"]
+		outputPayload["message_id"] = data["mid"]
+		outputPayload["page_title"] = data["t"]
+		outputPayload["page_url"] = data["p"]
+		outputPayload["browser_language"] = data["l"]
+		outputPayload["screen_size"] = data["sc"]
+		outputPayload["attributes"] = make(map[string]map[string]string)
+		outputPayload["traits"] = make(map[string]map[string]string)
+		attributesMap := outputPayload["attributes"].(map[string]map[string]string)
+		traitsMap := outputPayload["traits"].(map[string]map[string]string)
 
-	attributePattern := regexp.MustCompile(`^atrk(\d+)$`)
-	traitPattern := regexp.MustCompile(`^uatrk(\d+)$`)
-	for key, val := range data {
-		if isAttribute := attributePattern.MatchString(key); isAttribute {
-			attributeNum := key[4:]
-			attributeKey := val
-			attributeValue := data[fmt.Sprintf("atrv%v", attributeNum)]
-			attributeType := data[fmt.Sprintf("atrt%v", attributeNum)]
-			attributesMap[attributeKey] = map[string]string{
-				"value": attributeValue,
-				"type":  attributeType,
+		attributePattern := regexp.MustCompile(`^atrk(\d+)$`)
+		traitPattern := regexp.MustCompile(`^uatrk(\d+)$`)
+		for key, val := range data {
+			if isAttribute := attributePattern.MatchString(key); isAttribute {
+				attributeNum := key[4:]
+				attributeKey := val
+				attributeValue := data[fmt.Sprintf("atrv%v", attributeNum)]
+				attributeType := data[fmt.Sprintf("atrt%v", attributeNum)]
+				attributesMap[attributeKey] = map[string]string{
+					"value": attributeValue,
+					"type":  attributeType,
+				}
+			}
+			if isTrait := traitPattern.MatchString(key); isTrait {
+				traitNum := key[5:]
+				traitKey := val
+				traitValue := data[fmt.Sprintf("uatrv%v", traitNum)]
+				traitType := data[fmt.Sprintf("uatrt%v", traitNum)]
+				traitsMap[traitKey] = map[string]string{
+					"value": traitValue,
+					"type":  traitType,
+				}
 			}
 		}
-		if isTrait := traitPattern.MatchString(key); isTrait {
-			traitNum := key[5:]
-			traitKey := val
-			traitValue := data[fmt.Sprintf("uatrv%v", traitNum)]
-			traitType := data[fmt.Sprintf("uatrt%v", traitNum)]
-			traitsMap[traitKey] = map[string]string{
-				"value": traitValue,
-				"type":  traitType,
-			}
+
+		outputPayloadJSON, err := json.Marshal(outputPayload)
+		if err != nil {
+			log.Println("Error marshalling data:", err.Error())
 		}
+		resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(outputPayloadJSON))
+		if err != nil {
+			log.Println("Error sending request to webhook:", err.Error())
+		}
+		log.Println("Webhook response: ", resp.StatusCode)
+		resp.Body.Close()
+
+		// Webhook response handling placheolder
+
 	}
 }
--- main.go
+++ main.go
@@ -6,7 +6,17 @@ import (
 	"net/http"
 )
 
+// The dispatcher function will read from this channel
+var dataChan chan map[string]string
+
+const webhookURL = "https://webhook.site/your-webhook-endpoint"
+
 func main() {
+	// set up dispatcher func and the channel
+	dataChan = make(chan map[string]string, 100)
+	go dispatch()
+
+	// Server and router stuff
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /", requestHandler)
 	http.ListenAndServe(":8080", mux)
@@ -16,7 +26,7 @@ func requestHandler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	var incomingPayload map[string]string
 	json.NewDecoder(r.Body).Decode(&incomingPayload)
-	dispatch(incomingPayload)
+	dataChan <- incomingPayload
 	w.WriteHeader(http.StatusAccepted)
 	fmt.Println(incomingPayload)
 }
